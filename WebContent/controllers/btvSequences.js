c23e443665fa7aae070e90b66f2d50362f3d34ff
Josh Singer <josh.singer@glasgow.ac.uk>
2017-01-13 11:08:19 +0000

    incorporated updates by KIKI various stuff to do with date filtering etc.

--- WebContent/controllers/btvSequences.js
+++ WebContent/controllers/btvSequences.js
@@ -9,7 +9,7 @@ projectBrowser.controller('btvSequencesCtrl',
 
 			console.log("initializing btv sequences");
 
-			$scope.init("source.name = 'ncbi-curated' and excluded = null", 
+			$scope.init("source.name = 'ncbi-curated' and excluded = false", 
 					["source.name",
                      "sequenceID",
                      "gb_country_iso",
@@ -18,6 +18,7 @@ projectBrowser.controller('btvSequencesCtrl',
                      "gb_collection_year",
                      "gb_length",
                      "gb_create_date",
+                     "gb_update_date",
                      "gb_isolate",
                      "gb_host"] );
 			
@@ -29,7 +30,8 @@ projectBrowser.controller('btvSequencesCtrl',
   			
   			$scope.pagingContext.setSortableProperties([
   	            { property:"sequenceID", displayName: "NCBI Nucleotide ID" },
-  	            { property:"gb_create_date", displayName: "Creation Date" },
+  	            { property:"gb_create_date", displayName: "NCBI Entry Creation Date" },
+  	            { property:"gb_update_date", displayName: "NCBI Last Update Date" },
   	            { property:"gb_country_iso", displayName: "Country of Origin" },
   	            { property:"gb_collection_year", displayName: "Collection Year" },
   	            { property:"gb_isolate", displayName: "Isolate ID" },
@@ -42,7 +44,8 @@ projectBrowser.controller('btvSequencesCtrl',
 			$scope.pagingContext.setFilterProperties([
            		{ property:"sequenceID", displayName: "NCBI Nucleotide ID", filterHints: {type: "String"} },
           		{ property:"gb_length", displayName: "Sequence Length", filterHints: {type: "Integer"} },
-          		{ property:"gb_create_date", displayName: "Creation Date", filterHints: {type: "Date"} },
+          		{ property:"gb_create_date", displayName: "NCBI Entry Creation Date", filterHints: {type: "Date"} },
+  	            { property:"gb_update_date", displayName: "NCBI Last Update Date", filterHints: {type: "Date"} },
   	            { property:"gb_segment", displayName: "Segment", filterHints: {type: "Integer"} },
   	            { property:"gb_country_short", altProperties:["gb_country_iso"], displayName: "Country of Origin", filterHints: {type: "String"} },
   	            { property:"gb_host", displayName: "Host Species", filterHints: {type: "String"} },
