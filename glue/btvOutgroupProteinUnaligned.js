95d57f92dde973c1dde5592e011d3cf9d7613c6e
Josh Singer <josh.singer@glasgow.ac.uk>
2018-08-09 18:02:27 +0100

    deal with "broken CDS sequences".

--- glue/btvOutgroupProteinUnaligned.js
+++ glue/btvOutgroupProteinUnaligned.js
@@ -12,25 +12,68 @@ segToFeatureName["8"] = "NS2";
 segToFeatureName["9"] = "VP6";
 segToFeatureName["10"] = "NS3";
 
+// within each segment set there are various sequences for which deriving the protein sequence 
+// from the BTV_COMPL_SEG_NT_ using fastaProteinExporter does not work, producing a protein with 
+// stop codons in. This is for different reasons either to do with the sequence itself or how it aligns.
+// In these cases we use the fastaProteinAlignmentExporter instead, which produces an protein
+// sequence with gaps but no stop codons.
+
+var segToBrokenCDS = {};
+segToBrokenCDS["2"] = ["DQ191282"];
+segToBrokenCDS["3"] = ["JQ822250"];
+segToBrokenCDS["4"] = ["JX007926"];
+segToBrokenCDS["5"] = ["MF124296", "JQ822252"];
+segToBrokenCDS["7"] = ["DQ399835", "KP696666"];
+
 _.each(["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"], function(segNum) {
 	
-	var fastMapCombined;
+	var fastaMapOutgroup;
+	var fastaMapBtv;
+	var fastaMapBroken;
+	
+	var brokenCDSids = segToBrokenCDS[segNum];
 	
 	glue.inMode("module/fastaProteinExporter", function() {
-		var fastaMapOutgroup = glue.command(["export", "reference",
+		fastaMapOutgroup = glue.command(["export", "reference",
 		                                     "-f", segToFeatureName[segNum], 
 		                                     "-w", "sequence.isolate_segment = '"+segNum+"' and (name like '%OUTGROUP' or name like '%MASTER')", "-p"]);
-		var fastaMapBtv = glue.command(["export", "member", 
+
+		var whereClause = "sequence.source.name = 'ncbi-curated'";
+		if(brokenCDSids != null) {
+			var brokenCDSidSet = "('"+brokenCDSids.join("', '")+"')";
+			whereClause = whereClause + " and not sequence.sequenceID in "+brokenCDSidSet
+		}
+		
+		
+		fastaMapBtv = glue.command(["export", "member", 
 		                                "BTV_COMPL_SEG_NT_"+segNum, 
 		                                "-r", "REF_S"+segNum+"_MASTER", 
 		                                "-f", segToFeatureName[segNum], 
-		                                "-w", "sequence.source.name = 'ncbi-curated'", "-p"]);
-		
-		var combinedSeqs = _.union(fastaMapOutgroup.aminoAcidFasta.sequences, 
-				fastaMapBtv.aminoAcidFasta.sequences);
-		
-		fastMapCombined = {"aminoAcidFasta": { "sequences": combinedSeqs } };
+		                                "-w", whereClause, 
+		                                "-p"]);
 	});
+
+	if(brokenCDSids != null) {
+		var brokenCDSidSet = "('"+brokenCDSids.join("', '")+"')";
+
+		glue.inMode("module/fastaProteinAlignmentExporter", function() {
+			fastaMapBroken = glue.command(["export", 
+			                               "BTV_COMPL_SEG_NT_"+segNum, 
+			                               "-r", "REF_S"+segNum+"_MASTER", 
+			                               "-f", segToFeatureName[segNum], 
+			                               "-w", "sequence.source.name = 'ncbi-curated' and sequence.sequenceID in "+brokenCDSidSet, 
+			                               "-p"]);
+		});
+	} else {
+		fastaMapBroken = {"aminoAcidFasta": { "sequences": [] } }
+	}
+	
+	
+	var combinedSeqs = _.union(fastaMapOutgroup.aminoAcidFasta.sequences, 
+			fastaMapBtv.aminoAcidFasta.sequences, fastaMapBroken.aminoAcidFasta.sequences);
+	
+	var fastMapCombined = {"aminoAcidFasta": { "sequences": combinedSeqs } };
+
 	
 	glue.inMode("module/fastaUtility", function() {
 		glue.command({"save-amino-acid-fasta": {
