d6d45b365bb9373e1362221f1301d4e6afee4fcd
Josh Singer <josh.singer@glasgow.ac.uk>
2019-09-24 15:22:47 +0100

    project build updates and cluster caluclations / display tree improvements.

--- glue/generateClusterAnnotations.js
+++ glue/generateClusterAnnotations.js
@@ -3,49 +3,10 @@ var segments = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
 glue.command(["delete", "module", "-w", "name like 'btvClusterPicker%'"]);
 
 var clusterPickers = [
-	{ nodeThresholdType: "FBP", nodeThreshold:70, p_distance: 0.05 },
-	{ nodeThresholdType: "FBP", nodeThreshold:70, p_distance: 0.10 },
-	{ nodeThresholdType: "FBP", nodeThreshold:70, p_distance: 0.15 },
-	{ nodeThresholdType: "FBP", nodeThreshold:70, p_distance: 0.20 },
-	{ nodeThresholdType: "FBP", nodeThreshold:70, p_distance: 0.25 },
-	{ nodeThresholdType: "FBP", nodeThreshold:70, p_distance: 0.30 },
-	{ nodeThresholdType: "FBP", nodeThreshold:70, p_distance: 0.35 },
-	{ nodeThresholdType: "TBE", nodeThreshold:0.75, p_distance: 0.05 },
-	{ nodeThresholdType: "TBE", nodeThreshold:0.75, p_distance: 0.06 },
-	{ nodeThresholdType: "TBE", nodeThreshold:0.75, p_distance: 0.07 },
-	{ nodeThresholdType: "TBE", nodeThreshold:0.75, p_distance: 0.08 },
-	{ nodeThresholdType: "TBE", nodeThreshold:0.75, p_distance: 0.09 },
-	{ nodeThresholdType: "TBE", nodeThreshold:0.75, p_distance: 0.10 },
-	{ nodeThresholdType: "TBE", nodeThreshold:0.75, p_distance: 0.11 },
-	{ nodeThresholdType: "TBE", nodeThreshold:0.75, p_distance: 0.12 },
-	{ nodeThresholdType: "TBE", nodeThreshold:0.75, p_distance: 0.13 },
-	{ nodeThresholdType: "TBE", nodeThreshold:0.75, p_distance: 0.14 },
-	{ nodeThresholdType: "TBE", nodeThreshold:0.75, p_distance: 0.15 },
-	{ nodeThresholdType: "TBE", nodeThreshold:0.75, p_distance: 0.16 },
-	{ nodeThresholdType: "TBE", nodeThreshold:0.75, p_distance: 0.17 },
-	{ nodeThresholdType: "TBE", nodeThreshold:0.75, p_distance: 0.18 },
-	{ nodeThresholdType: "TBE", nodeThreshold:0.75, p_distance: 0.19 },
-	{ nodeThresholdType: "TBE", nodeThreshold:0.75, p_distance: 0.20 },
-	{ nodeThresholdType: "TBE", nodeThreshold:0.75, p_distance: 0.21 },
-	{ nodeThresholdType: "TBE", nodeThreshold:0.75, p_distance: 0.22 },
-	{ nodeThresholdType: "TBE", nodeThreshold:0.75, p_distance: 0.23 },
-	{ nodeThresholdType: "TBE", nodeThreshold:0.75, p_distance: 0.24 },
-	{ nodeThresholdType: "TBE", nodeThreshold:0.75, p_distance: 0.25 },
-	{ nodeThresholdType: "TBE", nodeThreshold:0.75, p_distance: 0.26 },
-	{ nodeThresholdType: "TBE", nodeThreshold:0.75, p_distance: 0.27 },
-	{ nodeThresholdType: "TBE", nodeThreshold:0.75, p_distance: 0.28 },
-	{ nodeThresholdType: "TBE", nodeThreshold:0.75, p_distance: 0.29 },
-	{ nodeThresholdType: "TBE", nodeThreshold:0.75, p_distance: 0.30 },
-	{ nodeThresholdType: "TBE", nodeThreshold:0.75, p_distance: 0.31 },
-	{ nodeThresholdType: "TBE", nodeThreshold:0.75, p_distance: 0.32 },
-	{ nodeThresholdType: "TBE", nodeThreshold:0.75, p_distance: 0.33 },
-	{ nodeThresholdType: "TBE", nodeThreshold:0.75, p_distance: 0.34 },
-	{ nodeThresholdType: "TBE", nodeThreshold:0.75, p_distance: 0.35 },
-	{ nodeThresholdType: "TBE", nodeThreshold:0.75, p_distance: 0.36 },
-	{ nodeThresholdType: "TBE", nodeThreshold:0.75, p_distance: 0.37 },
-	{ nodeThresholdType: "TBE", nodeThreshold:0.75, p_distance: 0.38 },
-	{ nodeThresholdType: "TBE", nodeThreshold:0.75, p_distance: 0.39 },
-	{ nodeThresholdType: "TBE", nodeThreshold:0.75, p_distance: 0.40 }
+	{ nodeThresholdType: "TBE", nodeThreshold:0.75, p_distance: 0.20, segments: ["1", "3", "4", "5", "7", "8", "9", "10"] },
+	{ nodeThresholdType: "TBE", nodeThreshold:0.75, p_distance: 0.24, segments: ["2"] },
+	{ nodeThresholdType: "TBE", nodeThreshold:0.75, p_distance: 0.33, segments: ["6"] },
+	{ nodeThresholdType: "TBE", nodeThreshold:0.75, p_distance: 0.35, segments: ["2"] }
 ];
 
 try {
@@ -98,64 +59,73 @@ _.each(clusterPickers, function(clusterPicker) {
 	});
 });
 
-glue.inMode("module/btvNexusExporter", function() {
-	glue.command(["load", "configuration", "modules/btvNexusExporter.xml"]);
-	_.each(clusterPickers, function(clusterPicker) {
-		var field = fieldForClusterPicker(clusterPicker);
-		glue.command(["add", "annotation-generator", "freemarker", field, "${sequence."+field+"!'-'}"]);
+_.each(["btvNexusExporter", "btvNexusExporter_Seg2", "btvNexusExporter_Seg6"], function(nexusExporter) {
+	glue.inMode("module/"+nexusExporter, function() {
+		glue.command(["load", "configuration", "modules/"+nexusExporter+".xml"]);
+		_.each(clusterPickers, function(clusterPicker) {
+			var field = fieldForClusterPicker(clusterPicker);
+			glue.command(["add", "annotation-generator", "freemarker", field, "${sequence."+field+"!'-'}"]);
+		});
 	});
 });
-
 
 var summaryString = "segment\tclusterPicker\tbootstrapType\tnodeThreshold\tpDistance\tnumSequences\tclusters\tsingletons\n";
 
 _.each(segments, function(segment) {
 	_.each(clusterPickers, function(clusterPicker) {
-		var whereClause = "sequence.source.name in ('ncbi-curated')";
-		if(segment == "6") {
-			// A bunch of Seg 6 genotypes group within PATAV+EHDV, so the internal node of these two cannot be used as an
-			// outgroup. Instead PATAV is used as an outgroup and so EHDV remains in the tree
-			whereClause = whereClause +" or (sequence.sequenceID = 'AM744982')";
-		}
-
-		var clusterResultObjs;
-		
-		glue.inMode("module/"+moduleNameForClusterPicker(clusterPicker), function() {
-		    clusterResultObjs = 
-		    	glue.tableToObjects(glue.command(["run", "cluster-picker", "field", "--alignmentName", "BTV_OUTG_CODON_"+segment]));
-		});
-
-		var clusters = 0;
-		var singletons = 0;
-
-		var field = fieldForClusterPicker(clusterPicker);
-		_.each(clusterResultObjs, function(clusterResultObj) {
-			glue.inMode("sequence/"+clusterResultObj.sourceName+"/"+clusterResultObj.sequenceID, function() {
-				if(clusterResultObj.clusterIndex == null) {
-					glue.command(["unset", "field", "--noCommit", field]);
-					singletons++;
-				} else {
-					glue.command(["set", "field", "--noCommit", field, clusterResultObj.clusterIndex]);
-					clusters = Math.max(clusters, clusterResultObj.clusterIndex);
-				}
+		if(_.contains(clusterPicker.segments, segment)) {
+			var whereClause = "sequence.source.name in ('ncbi-curated')";
+			if(segment == "6") {
+				// A bunch of Seg 6 genotypes group within PATAV+EHDV, so the internal node of these two cannot be used as an
+				// outgroup. Instead PATAV is used as an outgroup and so EHDV remains in the tree
+				whereClause = whereClause +" or (sequence.sequenceID = 'AM744982')";
+			}
+	
+			var clusterResultObjs;
+			
+			var moduleName = moduleNameForClusterPicker(clusterPicker);
+			glue.inMode("module/"+moduleName, function() {
+			    clusterResultObjs = 
+			    	glue.tableToObjects(glue.command(["run", "cluster-picker", "field", "--alignmentName", "BTV_OUTG_CODON_"+segment]));
 			});
-		});
-		glue.command(["commit"]);
-
-		summaryString += segment + "\t" + 
-			idForClusterPicker(clusterPicker) + "\t" +
-			clusterPicker.nodeThresholdType + "\t" +
-			clusterPicker.nodeThreshold + "\t" +
-			clusterPicker.p_distance + "\t" +
-			clusterResultObjs.length + "\t" +
-			clusters + "\t" +
-			singletons + "\n";
-		
-		glue.log("FINEST", "Set "+clusterPicker.module+" annotations for segment "+segment);
+	
+			var clusters = 0;
+			var singletons = 0;
+	
+			var field = fieldForClusterPicker(clusterPicker);
+			_.each(clusterResultObjs, function(clusterResultObj) {
+				glue.inMode("sequence/"+clusterResultObj.sourceName+"/"+clusterResultObj.sequenceID, function() {
+					if(clusterResultObj.clusterIndex == null) {
+						glue.command(["unset", "field", "--noCommit", field]);
+						singletons++;
+					} else {
+						glue.command(["set", "field", "--noCommit", field, clusterResultObj.clusterIndex]);
+						clusters = Math.max(clusters, clusterResultObj.clusterIndex);
+					}
+				});
+			});
+			glue.command(["commit"]);
+	
+			summaryString += segment + "\t" + 
+				idForClusterPicker(clusterPicker) + "\t" +
+				clusterPicker.nodeThresholdType + "\t" +
+				clusterPicker.nodeThreshold + "\t" +
+				clusterPicker.p_distance + "\t" +
+				clusterResultObjs.length + "\t" +
+				clusters + "\t" +
+				singletons + "\n";
+			
+			glue.log("FINEST", "Set "+moduleName+" annotations for segment "+segment);
+		}
 	});
 	
+	var nexusExporter = "btvNexusExporter";
+	if(segment == "2" || segment == "6") {
+		nexusExporter = "btvNexusExporter_Seg"+segment;
+	}
+	
 	glue.log("FINEST", "Exporting display tree for segment "+segment);
-	glue.inMode("module/btvNexusExporter", function() {
+	glue.inMode("module/"+nexusExporter, function() {
 		glue.command(["export", "tree", "BTV_OUTG_CODON_"+segment, 
 		              "-f", "trees/S"+segment+"_display.nexus"])
 	});
